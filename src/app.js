291fff4a6cff21d52aa651ea93508b341e203ee6
jeankory500 <jeankory123@gmail.com>
2023-04-13 00:38:07 +0000

    newSave

--- src/app.js
+++ src/app.js
@@ -1,11 +1,27 @@
 /* eslint-disable */
-import "bootstrap";
-import "./style.css";
-
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
-
-window.onload = function() {
+domain = () => {
   //write your code here
-  console.log("Hello Rigo from the console!");
-};
+  let pronouns = ["my", "your", "his", "her", "their"];
+  let adj = ["green", "happy", "cool", "bright", "fast"];
+  let nouns = ["dog", "cat", "house", "car", "book"];
+  let extentions = [".com", ".net", ".org"];
+
+  for (let i = 0; i < pronouns.length; i++) 
+    for (let j = 0; j < adj.length; j++) 
+      for (let k = 0; k < nouns.length; k++) 
+        for (let l = 0; l < extentions.length; l++) 
+        return(pronouns[i] + adj[j] + nouns[k] + extentions[l]);
+    };
+ console.log(domain())
+//let domainName = () => {
+//let pronoun = ["the", "our", "you", "anyone"];
+//let adj = ["great", "big", "small", "bright"];
+//let noun = ["jogger", "racoon", "dog", "cat", "cars"];
+
+//let pronounIndex = Math.floor(Math.random() * pronoun.length);
+//let adjIndex = Math.floor(Math.random() * adj.length);
+//let nounIndex = Math.floor(Math.random() * noun.length);
+//const domainName=
+//return pronoun[pronounIndex] + adj[adjIndex] + noun[nounIndex] + ".com";
+//};
+//console.log(domainName)
